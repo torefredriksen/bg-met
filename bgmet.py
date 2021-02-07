952284da41fbcaaafe33518025814b85c5229ca3
Tore Fredriksen <torefr@gmail.com>
2021-02-07 17:47:26 +0100

    Add a more precise met

--- bgmet.py
+++ bgmet.py
@@ -3,23 +3,28 @@
 # 2021
 # Tore Fredriksen, torefr@gmail.com
 
+
+
 # Kazaross-XG2 Match Equity Table
 # https://bkgm.com/articles/Keith/KazarossXG2MET/index.html
+# Higher precision
 met = [[0, 0, 0], \
-[0, 50.0, 67.7, 75.1, 81.4, 84.2, 88.7, 90.7], \
-[0, 32.3, 50.0, 59.9, 66.9,	74.4, 79.9, 84.2], \
-[0, 24.9, 40.1, 50.0, 57.6,	64.8, 71.1, 76.2], \
-[0, 18.6, 33.1, 42.9, 50.0, 57.7, 64.3, 69.9], \
-[0, 15.8, 25.6, 35.2, 42.3, 50.0, 56.6, 62.6], \
-[0, 11.3, 20.1, 28.9, 35.7, 43.4, 50.0, 56.3], \
-[0, 9.3, 15.8, 23.8, 30.1, 37.4, 43.7, 50.0]]
+[0, 50.000, 67.736, 75.076, 81.436, 84.179, 88.731, 90.724], \
+[0, 32.264, 50.000, 59.947, 66.870, 74.359, 79.940, 84.225], \
+[0, 24.924, 40.053, 50.000, 57.150, 64.795, 71.123, 76.209], \
+[0, 18.564, 33.130, 42.850, 50.000, 57.732, 64.285, 69.924], \
+[0, 15.821, 25.641, 35.205, 42.268, 50.000, 56.635, 62.638], \
+[0, 11.269, 20.060, 28.877, 35.715, 43.365, 50.000, 56.261], \
+[0, 9.276,  15.775, 23.791, 30.076, 37.362, 43.739, 50.000]]
 
 
 print (met)
 
+
 # Match winning chances for a given score
 def mwc (playeraway, opponentaway):
    return (met[playeraway][opponentaway])
+
 
 # Cubeless takepoint for a given score
 def cubelesstakepoint (playeraway, opponentaway):
@@ -35,8 +40,11 @@ def cubelesstakepoint (playeraway, opponentaway):
    takepoint = (risk / (risk + gain)*100)
    return takepoint
 
+
+
 print (mwc(3,2))
 
 # I get slightly different numbers than extreme gammon. Probably because they are using higher precision on the numbers in the met.
 # But this should be investigated before I go further
-print (cubelesstakepoint(7,4))
+print (cubelesstakepoint(4,6))
+
